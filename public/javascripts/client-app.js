59924c96b679d88d83e99e0459dea66345a832e5
John Ruggiero <jruggier@unca.edu>
2013-04-05 02:07:49 -0300

    Update client-app.js for lint

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,29 +1,24 @@
 var main = function () {
-	var happyWords = ["happy","ecstatic","joy","glad","fortunate","merry","cheerful"],
-		sadWords = ["sad","sorrowful","mournful","gloomy","woeful","upset","depressed"],
-		happyCount = 0,
-		sadCount = 0,
-		i; 
-		
-	function countTweets(response){
-		for (i = 0; i<happyWords.length; i++) {
-			if (happyWords[i]===response.key) {
-				happyCount = happyCount + parseInt(response.value);
-				
-			} else if (sadWords[i]===response.key) {
-				sadCount = sadCount + parseInt(response.value);
-			}
-		}
-		
-		
-		
-		$.getJSON("/counts.json", function (response) {
-			response.forEach(countTweets);
-			$("#happy").html(happyCount);
-			$("#sad").html(sadCount);
-			
-		});
-	};
-};
+        "use strict";
+        var happyWords = ["happy", "ecstatic", "joy", "glad", "fortunate", "merry", "cheerful"],
+            sadWords = ["sad", "sorrowful", "mournful", "gloomy", "woeful", "upset", "depressed"],
+            happyCount = 0,
+            sadCount = 0,
+            i;
+        function countTweets(response) {
+            for (i = 0; i < happyWords.length; i = i + 1) {
+                if (happyWords[i] === response.key) {
+                    happyCount = happyCount + parseInt(response.value);
+                } else if (sadWords[i] === response.key) {
+                    sadCount = sadCount + parseInt(response.value);
+                }
+            }
+            $.getJSON("/counts.json", function (response) {
+                response.forEach(countTweets);
+                $("#happy").html(happyCount);
+                $("#sad").html(sadCount);
+            });
+        };
+    };
 
 $(document).ready(main);
