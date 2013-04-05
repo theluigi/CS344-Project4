3de8862c4b2aaaebe2391fad6af28ade586e9d56
John Ruggiero <jruggier@unca.edu>
2013-04-05 01:56:16 -0300

    Update twitter.js for lint

--- twitter.js
+++ twitter.js
@@ -1,35 +1,30 @@
 var worker = function(trackWords) {
-	var redisClient = require("redis").createClient(),
-		twitter = require("ntwitter");
-		credentials = require("./credentials.js");
+        "use strict";
+        var redisClient = require("redis").createClient(),
+            twitter = require("ntwitter"),
+            credentials = require("./credentials.js"),
+            i,
 
-	var twit = new twitter({
-		consumer_key: credentials.consumer_key,
-		consumer_secret: credentials.consumer_secret,
-		access_token_key: credentials.access_token_key,
-		access_token_secret: credentials.access_token_secret
-	
-	});
-	
-twit.stream(
-	'statuses/filter'
-	{ track: trackWords },
-	function(stream) {
-		stream.on('data', function(tweet) {
-			console.log(tweet.text);
-			for (var i=0; i<trackWords.length; i++) {
-				if (tweet.text.indexOf(trackWords[i]) > -1) {
-					redisClient.incr(trackWords[i]);
-				}
-			}
-		});
-	}
-	
-	
-	
-);
-	
-	
-}
+            twit = new Twitter({
+                consumer_key: credentials.consumer_key,
+                consumer_secret: credentials.consumer_secret,
+                access_token_key: credentials.access_token_key,
+                access_token_secret: credentials.access_token_secret
+            });
 
+        twit.stream(
+            'statuses/filter',
+            { track: trackWords },
+            function(stream) {
+                stream.on('data', function(tweet) {
+                    console.log(tweet.text);
+                    for (i = 0; i < trackWords.length; i = i + 1) {
+                        if (tweet.text.indexOf(trackWords[i]) > -1) {
+                            redisClient.incr(trackWords[i]);
+                        }
+                    }
+                });
+            }
+        );
+    }
 module.exports = worker;
